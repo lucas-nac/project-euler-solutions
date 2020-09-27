3243d5351566ea795759779bff01c5338a2220b6
Lucas Amado <lucas.amadocordoba@gmail.com>
2020-09-27 03:44:19 -0300

    add solution 4

---
+++ solutions/Solution4.cs
@@ -0,0 +1,31 @@
+namespace project_euler_solutions.solutions
+{
+    public class Solution4 : ISolution
+    {
+        public string GetSolution()
+        {
+            var maxPalindrome = 0;
+            for (int i = 999; i > 100; i--) {
+                if (i*i < maxPalindrome) break;
+                for (int j = i; j > 100; j--) {
+                    var product = i*j;
+                    if (IsPalindrome(product) && product > maxPalindrome) {
+                        maxPalindrome = product;
+                    } 
+                }
+            }
+            return maxPalindrome.ToString();
+        }
+
+        private bool IsPalindrome(int n) {
+            var number = n.ToString();
+
+            for (int i = 0; i <= number.Length/2; i++) {
+                var opposedIndex = number.Length-1-i;
+                if (number[i] != number[opposedIndex]) return false;
+            }
+
+            return true;
+        }
+    }
+}

743fe099cc28d3523669673ad47a2b4d74067f95
Lucas Amado <lucas.amadocordoba@gmail.com>
2020-09-27 02:09:28 -0300

    add base program

---
+++ ISolution.cs
@@ -0,0 +1,7 @@
+namespace project_euler_solutions
+{
+    public interface ISolution
+    {
+        string GetSolution();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace project_euler_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.GetInterfaces().Any(y => y.Name == "ISolution"));
+            Console.WriteLine($"{solutions.Count()} solutions available");
+            if (solutions.Count() == 0) return;
+
+            Console.Write("Enter problem id: ");
+            if (!int.TryParse(Console.ReadLine(), out var id)) {
+                Console.WriteLine("Id could not be parsed");
+                return;
+            }
+
+            var solution = solutions.SingleOrDefault(s => s.Name == $"Solution{id}");
+            if (solution == null) {
+                Console.WriteLine("Requested solution doesn't exist.");
+                return;
+            }
+
+            Console.WriteLine(((ISolution)Activator.CreateInstance(solution)).GetSolution());
+        }
+    }
+}

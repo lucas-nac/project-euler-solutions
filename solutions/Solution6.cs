4c101371503e357395a2de9fc4a00c7b3c5c72e4
Lucas Amado <lucas.amadocordoba@gmail.com>
2020-09-27 13:38:32 -0300

    add solution 6

---
+++ solutions/Solution6.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+namespace project_euler_solutions.solutions
+{
+    public class Solution6 : ISolution
+    {
+        public string GetSolution()
+        {
+            var max = 100;
+            var sumOfSquares = (long)Enumerable.Range(1, max).Sum(n => Math.Pow(n, 2));
+            var squareOfSum = (long)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+            return (squareOfSum-sumOfSquares).ToString();
+        }
+    }
+}

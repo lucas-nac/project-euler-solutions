46fdb8ab1e47023c6b582c30835fb4fa68361f29
Lucas Amado <lucas.amadocordoba@gmail.com>
2020-09-27 22:00:32 -0300

    add tests for commons

---
+++ Tests/CommonsTests.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using ProjectEulerSolutions.Commons;
+using Xunit;
+
+namespace ProjectEulerSolutions.Tests
+{
+    public class CommonsTests
+    {
+        [Fact]
+        public void GetPrimeNumbersBelow()
+        {
+            Assert.Equal(25, PrimeNumbers.GetPrimeNumbersBelow(100).Count());
+        }
+
+        [Fact]
+        public void GetNthPrimeNumber()
+        {
+            Assert.Equal(7919, PrimeNumbers.GetNthPrimeNumber(1000));
+        }
+    }
+}

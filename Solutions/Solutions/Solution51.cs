884926b772e16c8d8fc839027e9161a4c966fda9
Lucas Amado <lucas.amadocordoba@gmail.com>
2022-02-16 01:50:49 -0300

    add solution 51

---
+++ Solutions/Solutions/Solution51.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectEulerSolutions.Commons;
+
+namespace ProjectEulerSolutions.Solutions
+{
+    public class Solution51 : ISolution
+    {
+        private static IList<IGrouping<int, long>> primeNumbersByDigits = PrimeNumbers.GetPrimeNumbersBelow(1000000).GroupBy(p => p.ToString().Count()).Where(d => d.Key >= 2).ToList();
+        
+        public string GetSolution()
+        {
+            foreach (var byDigits in primeNumbersByDigits)
+            {
+                var variations = getVariations(byDigits.Key).SkipLast(1).Skip(1);
+                var primes = byDigits.Select(p => p.ToString()).ToList();
+
+                var min = int.MaxValue;
+
+                foreach (var v in variations)
+                {
+                    var families = primes.GroupBy(p => maskIfValid(p, v)).Where(g => g.Key != string.Empty && g.Count() >= 8);
+
+                    foreach (var family in families)
+                    {
+                        var smallest = Convert.ToInt32(family.ElementAt(0));
+                        if (smallest < min) min = smallest;
+                    }                    
+                }
+
+                if (min != int.MaxValue) return min.ToString();
+            }
+
+            return "";
+        }
+
+        private static IList<string> getVariations(int n)
+        {
+            if (n == 1) return new List<string>() { "X", "0" };
+
+            var variations = new List<string>();
+            foreach (var v in getVariations(n-1))
+            {
+                variations.Add("X"+v);
+                variations.Add("0"+v);
+            }
+
+            return variations.ToList();
+        }
+
+        private static string maskIfValid(string number, string mask)
+        {
+            var masked = string.Empty;
+            var changed = string.Empty;
+
+            for (int i = 0; i < mask.Length; i++)
+            {
+                if (mask[i] == 'X') {
+                    masked += 'X';
+                    changed += number[i];
+                } else {
+                    masked += number[i];
+                }
+            }
+
+            return changed.Distinct().Count() == 1 ? masked : string.Empty;
+        }
+    }
+}
